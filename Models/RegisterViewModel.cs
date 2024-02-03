d56b27102487fe2147c7ba333a0ef61f64f4c441
WAHABTURK11 <wahabturk11@gmail.com>
2024-02-04 00:14:07 +0500

    Initial Commit

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetailManager.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password and confirm password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
